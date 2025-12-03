0474999db9f0ff02e630f7fcf00a7df3ac2e9eac
miku393911 <mik390625@gmail.com>
2025-12-03 19:16:13 +0900

    2番まで解いた

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react"
+
+/*export function App (){
+  const [count, setCount] = useState(0)
+  const onClick = ( () =>  //先に変数として挙動を書いて、のちにその変数を呼び出すみたいな
+    setCount(count+1)
+  )
+  return(
+    <button onClick={onClick}>Click! {count}</button>
+  )
+}*/
+
+/*export function App (){
+  const [toggle, setToggle] = useState(true);
+  return ( //変数として挙動を書かずに、そのまま代入する　ボタンが押されるたび、スイッチの入れ替え（！で反転も可能）
+    <button onClick={ ()=> setToggle(!toggle) }>{toggle ? "ON" : "OFF"}</button> //実際に表示される文字は、三項演算子で簡単に
+  )
+}*/
+
