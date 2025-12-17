550a43fe2f4e86f9f6fa1561c78d47a7943686b2
miku <miku@example.com>
2025-12-17 20:00:28 +0900

    complete ./Q01_Count.jsx

--- src/components/vol1/Q01_Count.jsx
+++ src/components/vol1/Q01_Count.jsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
 
-export function Q01_Count() {
+/*export function Q01_Count() {
     const [count, setCount] = useState(0);
     const onClick = () => setCount(count + 1);
     return (
         <button onClick={onClick}>{count}</button>
     );
-}
+}*/
+
+export function Q01_Count (){
+    const [count, setCount] = useState(0);
+    const onClickCount = () => setCount( (prev) => prev + 1);
+    return <button onClick = {onClickCount}>{count}</button> //アロー関数で書く場合は、引数を取る時
+} //最適化された解答
