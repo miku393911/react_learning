74140b3b760f737e41ae4b8aa87499b3a4282d97
miku <miku@example.com>
2025-12-17 20:33:13 +0900

    complete ./Q02_Toggle.jsx

--- src/components/vol1/Q02_Toggle.jsx
+++ src/components/vol1/Q02_Toggle.jsx
@@ -1,8 +1,15 @@
 import { useState } from "react";
 
-export function Q02_Toggle() {
+/*export function Q02_Toggle() {
     const [toggle, setToggle] = useState(true);
     return (
         <button onClick={() => setToggle(!toggle)}>{toggle ? "ON" : "OFF"}</button>
-    );
-}
+    ); //引数を取るので、アロー関数で表記する、しかし今回のコードだと、レンダリングされるたびに新しい関数を作ってしまう
+}*/
+export function Q02_Toggle (){
+    const [isActive, setIsActive] = useState(false);
+    const onClickActive = () => {
+        setIsActive( (prev) => !prev); //前の値を使って更新したい時はupdate関数をう
+    }
+    return <button onClick={onClickActive}>{isActive ? "ON" : "OFF"}</button>
+} //関数を入れる方がコンパクトで良い、今回のように短ければインラインで直接代入しても良い
