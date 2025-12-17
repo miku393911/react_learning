37495385d3562f182b7ef6676796b2a1e8be343f
miku <miku@example.com>
2025-12-17 19:20:24 +0900

    complete q32_Favorite

--- src/components/vol2/Q32_Favorite.jsx
+++ src/components/vol2/Q32_Favorite.jsx
@@ -1,11 +1,8 @@
 import { useState } from "react";
 
-export function Q01_Favorite() {
-    // TODO: お気に入り状態を管理するステートを作成
-    // ボタンをクリックすると、お気に入りの状態が切り替わる
-    // お気に入りの場合は「❤️ お気に入り」、そうでない場合は「🤍 お気に入り」を表示
-
+export function Q32_Favorite() {
+    const [isFavorite, setIsFavorite] = useState(false); //お気に入り状態を管理
     return (
-        <button>🤍 お気に入り</button>
-    );
+        <button onClick={ () => setIsFavorite(!isFavorite)}>{isFavorite ? "❤️ お気に入り" : "🤍 お気に入り"}</button>
+    ); //ボタンをクリックするとお気に入りの状態が切り替わる、お気に入りの場合は「❤️ お気に入り」、そうでない場合は「🤍 お気に入り」を表示
 }
