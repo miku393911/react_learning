cc85c6ab46210c872cf6327e202c351fc7fcbe02
miku <miku@example.com>
2025-12-21 22:55:08 +0900

    edit Q07_Login.jsx

--- src/components/vol1/Q07_MaxNumber.jsx
+++ src/components/vol1/Q07_MaxNumber.jsx
@@ -12,7 +12,7 @@ import { useState } from "react";
     );
 }*/
 //正解
-export function Q07_MaxNumber(){
+/*export function Q07_MaxNumber(){
     const [count, setCount] = useState(""); //現在の値は文字として扱う
     const [maxNumber, setMaxNumber] = useState(0); //最大値
     return ( //<input>タグの値の状態を保持するuseState()の値は初期値を０にしても元が文字列なので型を合わせるためにNumber()を使う
@@ -22,4 +22,22 @@ export function Q07_MaxNumber(){
             <p>最大値: {maxNumber}</p>
         </>
     ) //Math.max(前の値, 次の値)で最大値を比較することができる
+}*/
+// この書き方も初心者向け（条件分岐
+export function Q07_MaxNumber(){
+    const [number, setNumber] = useState("");
+    const [maxNumber, setMaxNumber] = useState(0);
+    const onClickUpdate = () => {
+        const newNumber = Number(number);
+        if(newNumber > maxNumber){
+            setMaxNumber(newNumber);
+        }
+    }
+    return (
+        <>
+            <input value={number} onChange={ (e) => setNumber(e.target.value)} placeholder="数値を入力してください" />
+            <button onClick={ () => onClickUpdate()}>更新</button>
+            <p>最大値: {maxNumber}</p>
+        </>
+    )
 }
