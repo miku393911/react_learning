748acd6cc7f6552b2b096f8d81409c2da9569af0
miku <miku@example.com>
2025-12-23 16:40:55 +0900

    edit ./Q12_UserCardList.jsx

--- src/components/vol1/Q12_UserCardList.jsx
+++ src/components/vol1/Q12_UserCardList.jsx
@@ -1,4 +1,4 @@
-function UserCard(props) {
+/*function UserCard(props) {
     return (
         <li>名前: {props.name}（年齢: {props.age}歳）</li>
     );
@@ -16,4 +16,20 @@ export function Q12_UserCardList() {
             </ul>
         </>
     );
+}*/
+
+// 現場向き
+function UserCard( {name, age} ) { //引数を分割代入で短く
+    return <li>名前: {name}（年齢: {age} 歳） </li>; //return内が1行の場合は()不要
+}
+export function Q12_UserCardList() {
+    const users = [
+        {id: 1, name: "大谷", age: 17},
+        {id: 2, name: "小泉", age: 18},
+    ];
+    return (
+        <ul>
+            {users.map( (user) => (<UserCard key={user.id} {...user} />))} 
+        </ul>
+    ) // 分割代入 + props名と引数名が同じだから省略可能
 }
