20df23f3c1ae015f6c20f9da978a2f3b39a97d6f
miku <miku@example.com>
2025-12-23 18:01:24 +0900

    edit Q13

--- src/components/vol1/Q13_FilterActive.jsx
+++ src/components/vol1/Q13_FilterActive.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export function Q13_FilterActive() {
+/*export function Q13_FilterActive() {
     const [isActive, setIsActive] = useState(true);
     const users = [
         { name: "miku", isActive: true },
@@ -22,4 +22,25 @@ export function Q13_FilterActive() {
             </ul>
         </>
     );
+}*/
+
+// 現場向き
+const users = [ // 動的データは、関数外に書くのが定石
+    {id: 1, name: "miku", isActive: true},
+    {id: 2, name: "yuka", isActive: true},
+    {id: 3, name: "yuuta", isActive: false},
+]
+export function Q13_FilterActive() {
+    const [isActive, setIsActive] = useState(false);
+    const newActive = isActive ? users.filter( (user) => user.isActive) : users; //isActiveがtrueかfalseかの判定
+    return ( //e.target.checkedで、現在のチェックボックスの状態をそのままStateに反映する意味になる
+        <>
+            <label>
+                <input type="checkbox" checked={isActive} onChange={ (e) => setIsActive(e.target.checked)} />
+            </label>
+            <ul>
+                {newActive.map( (user) => <li key={user.id}>{user.name}</li>)}
+            </ul>
+        </>
+    ) //isActiveを判定した変数をmapでもう1度配置するイメージ
 }
