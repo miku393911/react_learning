25f9ae173159993fee0a062e2b31cb11981d5fea
miku <miku@example.com>
2025-12-25 08:46:39 +0900

    add Q23

--- src/components/vol1/Q23_EffectStatus.jsx
+++ src/components/vol1/Q23_EffectStatus.jsx
@@ -3,14 +3,14 @@ import { useState, useEffect } from "react";
 export function Q23_EffectStatus() {
     const [status, setStatus] = useState("読み込み中...");
     useEffect(() => {
-        const timer = setTimeout(() => {
+        const timer = setTimeout(() => {  //setTimeout()メソッドは、一定時間が経過後、1度だけ処理を実行する関数
             setStatus("データ読み込み完了！");
         }, 3000)
-        return () => clearTimeout(timer)
-    }, [])
+        return () => clearTimeout(timer)  // クリーンアップ関数
+    }, []);
     return (
         <>
             <p style={{ color: status === "データ読み込み完了！" ? "blue" : "black" }}>{status}</p>
         </>
-    );
+    ); //試しに色を変えてみた
 }
