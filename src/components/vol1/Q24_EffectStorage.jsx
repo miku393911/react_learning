6d2187d6e781724b372d48c4e3d94a1a0f5616b5
miku <mik390625@gmail.com>
2025-12-25 13:05:43 +0900

    edit Q24

--- src/components/vol1/Q24_EffectStorage.jsx
+++ src/components/vol1/Q24_EffectStorage.jsx
@@ -1,9 +1,9 @@
 import { useState, useEffect } from "react";
 
 export function Q24_EffectStorage() {
-    const [input, setInput] = useState(localStorage.getItem("input") || "");
+    const [input, setInput] = useState(localStorage.getItem("input") || ""); // setItemとセットで使われ、データを取得する役割
     useEffect(() => {
-        localStorage.setItem("input", input);
+        localStorage.setItem("input", input); // getItemとsetで使われ、データを保存する役割 / keyは"input"
     }, [input])
     return (
         <>
