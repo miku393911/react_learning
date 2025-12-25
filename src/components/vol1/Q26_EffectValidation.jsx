ae40b9e000452bf8294fceca79e366f22d5244ad
miku <mik390625@gmail.com>
2025-12-25 16:03:58 +0900

    edit Q26

--- src/components/vol1/Q26_EffectValidation.jsx
+++ src/components/vol1/Q26_EffectValidation.jsx
@@ -3,8 +3,8 @@ import { useState, useEffect } from "react";
 export function Q26_EffectValidation() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [errorEmail, setErrorEmail] = useState("")
-    const [errorPassword, setErrorPassword] = useState("")
+    const [errorEmail, setErrorEmail] = useState("");  // メールアドレスのエラーメッセージを表示する場合の状態保存
+    const [errorPassword, setErrorPassword] = useState("");  // パスワードのエラーメッセージを表示する場合の状態保存
     useEffect(() => {
         setErrorEmail(email.length === 0 ? "" : (email.includes("@") ? "" : "メールアドレスが正しくありません"))
         setErrorPassword(password.length === 0 ? "" : (password.length >= 8 ? "" : "パスワードが正しくありません"))
@@ -18,3 +18,6 @@ export function Q26_EffectValidation() {
         </>
     );
 }
+
+/* 現場向き
+<input type="password">でパスワードを入力する際、伏せ字で表現してくれる */
