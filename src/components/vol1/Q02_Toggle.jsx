59603c808efdf94ad2b49d1a0b474f301de5f5c7
miku <mik390625@gmail.com>
2025-12-25 22:49:16 +0900

    edit Q02

--- src/components/vol1/Q02_Toggle.jsx
+++ src/components/vol1/Q02_Toggle.jsx
@@ -9,7 +9,7 @@ import { useState } from "react";
 export function Q02_Toggle (){
     const [isActive, setIsActive] = useState(false);
     const onClickActive = () => {
-        setIsActive((prev) => !prev); //前の値を使って更新したい時はupdate関数をう
+        setIsActive((prev) => !prev); //前の値を使って更新したい時はupdate関数
     }
     return <button onClick={onClickActive}>{isActive ? "ON" : "OFF"}</button>
 } //関数を入れる方がコンパクトで良い、今回のように短ければインラインで直接代入しても良い
