785e7e89a82dcb331a8dc7822c4a3dc191a650b3
miku <mik390625@gmail.com>
2025-12-25 17:05:34 +0900

    edit Q27

--- src/components/vol1/Q27_Popup.jsx
+++ src/components/vol1/Q27_Popup.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 
 export function Q27_Popup() {
-    const [isOpen, setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);  // 画面のポップアップの状態
     return (
         <>
             <button onClick={() => setIsOpen(true)}>開く</button>
-            {isOpen && (
+            {isOpen && (  // isOpenがtrueだったら、ポップアップを開く + 閉じるボタンを表示
                 <div style={{ backgroundColor: "lightblue" }}>
                     <p>ポップアップです</p>
                     <button onClick={() => setIsOpen(false)}>閉じる</button>
