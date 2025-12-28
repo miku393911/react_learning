af0a390d6946884acd2aa03fc95cab1da0d79448
miku <mik390625@gmail.com>
2025-12-28 23:17:59 +0900

    sedit Q36

--- src/components/vol2/Q36_MathMax.jsx
+++ src/components/vol2/Q36_MathMax.jsx
@@ -9,8 +9,10 @@ export function Q36_MathMax() {
     const onChange = (e) => { // 本来なら<input>タグの属性として書く部分
         const newNumber = Number(e.target.value);  // 型をを揃える
         setNumber(newNumber);  // それをsetする
-        const newMax = Math.max(maxNumber, newNumber);  // 最大値を比較（最大値、比較する値）
-        setMaxNumber(newMax);  // それをsetする
+        //const newMax = Math.max(maxNumber, newNumber);  // 最大値を比較（最大値、比較する値）
+        if(newNumber > maxNumber){  // 上の1行を消してif文で書いても良い
+            setMaxNumber(newNumber);  // それをsetする
+        }
     }
     return (
         <>
