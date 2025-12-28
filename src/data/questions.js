61d8ea99b066a1b574b40faadc10f6606ba31790
miku <mik390625@gmail.com>
2025-12-29 04:33:49 +0900

    edit Q37

--- src/data/questions.js
+++ src/data/questions.js
@@ -37,8 +37,8 @@ import { Q32_NumberAdd } from "../components/vol2/Q32_NumberAdd";
 import { Q33_Checkbox } from "../components/vol2/Q33_Checkbox";
 import { Q34_Color } from "../components/vol2/Q34_Color";
 import { Q35_ValueLength } from "../components/vol2/Q35_ValueLength";
-import { Q36_Placeholder } from "../components/vol2/Q36_Placeholder";
-import { Q37_Placeholder } from "../components/vol2/Q37_Placeholder";
+import { Q36_MathMax } from "../components/vol2/Q36_MathMax";
+import { Q37_ClickButton } from "../components/vol2/Q37_ClickButton";
 import { Q38_Placeholder } from "../components/vol2/Q38_Placeholder";
 import { Q39_Placeholder } from "../components/vol2/Q39_Placeholder";
 import { Q40_Placeholder } from "../components/vol2/Q40_Placeholder";
@@ -110,8 +110,8 @@ export const questionList = [
     { id: "Q33", component: Q33_Checkbox, label: "Q33: Checkbox", volume: 2 },
     { id: "Q34", component: Q34_Color, label: "Q34: Color", volume: 2 },
     { id: "Q35", component: Q35_ValueLength, label: "Q35: Value Length", volume: 2 },
-    { id: "Q36", component: Q36_Placeholder, label: "Q36: 最大値のリアルタイム追跡", volume: 2 },
-    { id: "Q37", component: Q37_Placeholder, label: "Q37: 特定要素のみ色変更", volume: 2 },
+    { id: "Q36", component: Q36_MathMax, label: "Q36: Math Max", volume: 2 },
+    { id: "Q37", component: Q37_ClickButton, label: "Q37: Click Button", volume: 2 },
     { id: "Q38", component: Q38_Placeholder, label: "Q38: 配列へのスプレッド追加", volume: 2 },
     { id: "Q39", component: Q39_Placeholder, label: "Q39: Enterキー操作(KeyDown)", volume: 2 },
     { id: "Q40", component: Q40_Placeholder, label: "Q40: 配列Stateの全クリア", volume: 2 },
