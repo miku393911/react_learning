65774ec58614506cb53e5d6e84a54a61502ee9e6
miku <mik390625@gmail.com>
2026-01-11 20:12:06 +0900

    add Q45

--- src/data/questions.js
+++ src/data/questions.js
@@ -46,7 +46,7 @@ import { Q41_TodoDecoration } from "../components/vol2/Q41_TodoDecoration";
 import { Q42_DataIncludes} from "../components/vol2/Q42_DataIncludes";
 import { Q43_ObjectName } from "../components/vol2/Q43_ObjectName";
 import { Q44_InputTransform } from "../components/vol2/Q44_InputTransform";
-import { Q45_Placeholder } from "../components/vol2/Q45_Placeholder";
+import { Q45_CountDocumentTitle } from "../components/vol2/Q45_CountDocumentTitle";
 import { Q46_Placeholder } from "../components/vol2/Q46_Placeholder";
 import { Q47_Placeholder } from "../components/vol2/Q47_Placeholder";
 import { Q48_Placeholder } from "../components/vol2/Q48_Placeholder";
@@ -121,7 +121,7 @@ export const questionList = [
     { id: "Q42", component: Q42_DataIncludes, label: "Q42: Data Includes", volume: 2 },
     { id: "Q43", component: Q43_ObjectName, label: "Q43: Object Name", volume: 2 },
     { id: "Q44", component: Q44_InputTransform, label: "Q44:Input Transform", volume: 2 },
-    { id: "Q45", component: Q45_Placeholder, label: "Q45: タブ名連動(useEffect)", volume: 2 },
+    { id: "Q45", component: Q45_CountDocumentTitle, label: "Q45: Count Document Title", volume: 2 },
     { id: "Q46", component: Q46_Placeholder, label: "Q46: リサイズ監視(クリーンアップ)", volume: 2 },
     { id: "Q47", component: Q47_Placeholder, label: "Q47: 自動消去メッセージ(タイマー)", volume: 2 },
     { id: "Q48", component: Q48_Placeholder, label: "Q48: LocalStorageへの保存", volume: 2 },
